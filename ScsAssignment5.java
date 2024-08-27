1612c4f77fb12a46a36c823537c578f3774174aa
Bash <gousetough@gmail.com>
2024-08-27 15:42:26 +0530

    Create ScsAssignment5.java

---
+++ ScsAssignment5.java
@@ -0,0 +1,35 @@
+class Tester {
+	public static void main(String[] args) {
+		// Implement your code here 
+		int accountNumber = '';
+		int salary = '';
+		int accountBalance = ''
+		char loanType = '';
+		int loanAmountExpected = '';
+		int emisExpected = '';
+		int eligibleLoanAmount, eligibleEmis;
+		
+		if (String.valueof(accountNumber).length != 4 || String.valueof(accountNumber).charAt(0) != '1'){
+		    System.out.println("Error : Invalid account number");
+		    return;
+		}
+		if (accountBalance < 1000){
+		    System.out.println("Insufficiant balance");
+		}
+		if (salary > 25000 && loanType == 'car'){
+		    eligibleLoanAmount = 500000;
+		    eligibleEmis = 36;
+		}
+		if (salary > 50000 && loanType == 'house'){
+		    eligibleLoanAmount = 500000;
+		    eligibleEmis = 6000000;
+		}
+		if (salary > 75000 && loanType == 'business'){
+		    eligibleLoanAmount = 75000000;
+		    eligibleEmis = 84;
+		}
+		System.out.println("eligibleLoanAmount: " +eligibleLoanAmount);
+		System.out.print("eligibleEmis: " +eligibleEmis);
+	}
+}
+
